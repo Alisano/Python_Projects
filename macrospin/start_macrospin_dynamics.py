21beb801549bf2405fa06a21140626fd02040405
Alisano <alexcoretex@gmail.com>
2019-02-12 06:40:14 -0800

    Add files via upload

---
+++ macrospin/start_macrospin_dynamics.py
@@ -0,0 +1,17 @@
+# coding: utf8
+# Продукт разработали А.В.Попов и Г.Д.Демин, Все права защищены (c).
+# Програмное обеспечение защищено патентом Российской Федерации
+# The product was developed by A.V.Popov and G.D.Demin. All rights reserved (c).
+# Software is protected by a patent of the Russian Federation
+import matplotlib.pyplot as plt
+from functions_list import *
+from constants import*
+import time
+main()
+# -----------------результаты моделирования----------------
+# магнитная динамика намагниченности на сфере
+# графики проекций намагниченности mx, my, mz (переключ-е по оси OY)
+
+print 'Time complete program: '+str(time.clock())+' second'
+#plt.legend('mx', 'my', 'mz')
+
